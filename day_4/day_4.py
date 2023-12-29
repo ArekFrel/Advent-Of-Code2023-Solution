3552fecf11d73484705e6989b7c3fe576c43478e
ArekFrel <arkadiuszfrel@gmail.com>
2023-12-29 13:15:13 +0100

    3rd and 4th day solved

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,7 +1,7 @@
 DAY = '04'
 FILE = f'../inputs/input_{DAY}.txt'
 # FILE = f'../inputs/test_input_{DAY}.txt'
-SOLVE_PART = 1
+SOLVE_PART = 2
 with open(FILE, 'r', encoding='utf-8') as my_input:
     ARRAY = [line for line in my_input]
 
@@ -30,16 +30,70 @@ def solve_part_1():
     print(f'Result of day {DAY} part {SOLVE_PART} is {sum(score)}.')
 
 
+class Card:
 
+    cards = []
 
+    def __init__(self, index, shoots, winning_numbers):
+        self.index = index
+        self.shoots = shoots
+        self.winning_numbers = winning_numbers
+        self.counter = 1
+        self.scratched = False
+        Card.cards.append(self)
 
+    def count_points(self):
+        points = len(list(filter(lambda x: x in self.winning_numbers, self.shoots)))
+        return points
 
+    def add_cards(self):
+        if self.index == 5:
+            a = 0
+        number_of_cards_to_add = self.count_points()
+        if number_of_cards_to_add > 0:
+            for i in range(self.index, self.index + number_of_cards_to_add ):
+                Card.card_by_id(i).counter += self.counter
+            self.mark_as_scratched()
+
+    def mark_as_scratched(self):
+        self.scratched = True
+
+    @staticmethod
+    def card_by_id(index):
+        if index > len(Card.cards):
+            return None
+        return Card.cards[index]
+
+    @staticmethod
+    def scratch():
+        for card in Card.cards:
+            if not card.scratched:
+                card.add_cards()
+
+    @staticmethod
+    def count_cards():
+        return sum(list(map(lambda x: x.counter, Card.cards)))
 
 
 def solve_part_2():
-    pass
 
+    def create_cards():
+        for index, line in enumerate(ARRAY, 1):
+            line = remove_double_space(line)
+            line = line[line.index(':') + 1:]
+            winning_numbers, shoots = line.split('|')
+            winning_numbers = [int(_) for _ in winning_numbers.strip().split(' ')]
+            shoots = [int(_) for _ in shoots.strip().rstrip().split(' ')]
+            Card(index, winning_numbers, shoots)
 
+    def calculate():
+        Card.scratch()
+        return Card.count_cards()
+
+    create_cards()
+    result = calculate()
+    print(f'Result of day {DAY} part {SOLVE_PART} is {result}.')
+    return result
 
 
 def main():
