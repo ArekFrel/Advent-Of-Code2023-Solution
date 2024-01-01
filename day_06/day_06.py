a8a1015bfb0d2530cab0ae885cbb68bd8547ec1d
ArekFrel <arkadiuszfrel@gmail.com>
2024-01-01 15:23:33 +0100

    6th day solved

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -3,26 +3,19 @@ import math
 DAY = __file__[-5:-3]
 FILE = f'../inputs/input_{DAY}.txt'
 # FILE = f'../inputs/test_input_{DAY}.txt'
-SOLVE_PART = 1
+SOLVE_PART = 2
 
 
 def get_data(text):
-    while '  ' in text:
-        text = text = text.replace('  ', ' ')
-    return [int(num) for num in text.split(' ')]
+    if SOLVE_PART == 1:
+        while '  ' in text:
+            text = text.replace('  ', ' ')
+        return [int(num) for num in text.split(' ')]
 
-
-def solve():
-    with open(FILE, 'r', encoding='utf-8') as my_input:
-        times = get_data(my_input.readline().split(':')[1].strip())
-        records = get_data(my_input.readline().split(':')[1].strip())
-
-    pairs = list(zip(times, records))
-    values = []
-    for pair in pairs:
-        values.append(calc_race(pair))
-
-    print(f'Result of day {DAY} part {SOLVE_PART} is {math.prod(values)}.')
+    if SOLVE_PART == 2:
+        while ' ' in text:
+            text = text.replace(' ', '')
+        return int(text)
 
 
 def calc_race(pair):
@@ -40,13 +33,31 @@ def calc_race(pair):
     return ways_to_win
 
 
+def solve_01():
+    with open(FILE, 'r', encoding='utf-8') as my_input:
+        times = get_data(my_input.readline().split(':')[1].strip())
+        records = get_data(my_input.readline().split(':')[1].strip())
+
+    pairs = list(zip(times, records))
+    values = []
+    for pair in pairs:
+        values.append(calc_race(pair))
+
+    print(f'Result of day {DAY} part {SOLVE_PART} is {math.prod(values)}.')
+
+
 def solve_02():
-    pass
+    with open(FILE, 'r', encoding='utf-8') as my_input:
+        time = get_data(my_input.readline().split(':')[1].strip())
+        record = get_data(my_input.readline().split(':')[1].strip())
+    pair = (time, record)
+    result = calc_race(pair)
+    print(f'Result of day {DAY} part {SOLVE_PART} is {result}.')
 
 
 def main():
     if SOLVE_PART == 1:
-        solve()
+        solve_01()
     if SOLVE_PART == 2:
         solve_02()
 
