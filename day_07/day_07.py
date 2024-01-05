0b4fd2cfb1eeff4f24c24621ac7192b61c3c333a
ArekFrel <arkadiuszfrel@gmail.com>
2024-01-05 22:45:41 +0100

    7th day finished

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -1,61 +1,137 @@
-import math
+import string
 
 DAY = __file__[-5:-3]
-# FILE = f'../inputs/input_{DAY}.txt'
-FILE = f'../inputs/test_input_{DAY}.txt'
+FILE = f'../inputs/input_{DAY}.txt'
+# FILE = f'../inputs/test_input_{DAY}.txt'
 SOLVE_PART = 2
 
 
-def get_data(text):
-    if SOLVE_PART == 1:
-        while '  ' in text:
-            text = text.replace('  ', ' ')
-        return [int(num) for num in text.split(' ')]
+class Hand:
 
-    if SOLVE_PART == 2:
-        while ' ' in text:
-            text = text.replace(' ', '')
-        return int(text)
+    type_of_cards = [
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'T',
+        'J',
+        'Q',
+        'K',
+        'A',
+    ]
+    all_hands = []
 
+    def __init__(self, cards, bid):
+        self.cards = cards
+        self.bid = int(bid)
+        self.type = self.type_of_hand()
+        Hand.all_hands.append(self)
+        self.rank = None
+        self.coded_hand = self.code_name()
 
-def calc_race(pair):
-    time, record = pair
-    pivot = math.ceil(time / 2)
-    not_even = not((time + 1) % 2 == 0)
-    ways_to_win = 0
-    while pivot * (time - pivot) > record:
-        ways_to_win += 1
-        pivot += 1
-    if not_even:
-        ways_to_win = ways_to_win * 2 - 1
-    else:
-        ways_to_win *= 2
-    return ways_to_win
+    def code_name(self):
+        values = [sign for sign in string.ascii_lowercase[0:len(Hand.type_of_cards) + 1]]
+        dictionary = {k: v for k, v in zip(Hand.type_of_cards, values)}
+        return ''.join([dictionary.get(sign) for sign in self.cards])
+
+    # @staticmethod
+    # def compare_card(card_1, card_2):
+    #     rank_card_1 = Hand.type_of_cards.index(card_1)
+    #     rank_card_2 = Hand.type_of_cards.index(card_2)
+    #
+    #     if rank_card_1 < rank_card_2:
+    #         return card_1
+    #     if rank_card_1 > rank_card_2:
+    #         return card_2
+    #     if rank_card_1 == rank_card_2:
+    #         return 0
+
+    def type_of_hand(self):
+        diff_cards = list(set([x for x in self.cards]))
+        card_counter = []
+        for diff_card in diff_cards:
+            if SOLVE_PART == 2:
+                if diff_card != 'J':
+                    card_counter.append(self.cards.count(diff_card))
+                else:
+                    card_counter.append(0)
+            else:
+                card_counter.append(self.cards.count(diff_card))
+        if SOLVE_PART == 2:
+            if self.cards.count('J') > 0:
+                card_counter.sort(reverse=True)
+                if len(card_counter) == 0:
+                    a = 0
+                card_counter[0] += self.cards.count('J')
+
+        if 5 in card_counter:
+            return 0
+        if 4 in card_counter:
+            return 1
+        if 3 in card_counter and 2 in card_counter:
+            return 2
+        if 3 in card_counter and 1 in card_counter:
+            return 3
+        if card_counter.count(2) == 2:
+            return 4
+        if 2 in card_counter and card_counter.count(1) == 3:
+            return 5
+        if card_counter.count(1) == 5:
+            return 6
+
+    @staticmethod
+    def rank_of_hand():
+        hand_rank = 1
+        for type in list(range(6, -1, -1)):
+            one_type_hands = list(filter(lambda x: x.type == type, Hand.all_hands))
+            one_type_hands.sort(key=lambda x: x.coded_hand)
+            for hand in one_type_hands:
+                hand.rank = hand_rank
+                hand_rank += 1
+
+    @staticmethod
+    def count_bid():
+        result = 0
+        for hand in Hand.all_hands:
+            result += hand.rank * hand.bid
+        return result
 
 
 def solve_01():
     with open(FILE, 'r', encoding='utf-8') as my_input:
-        times = get_data(my_input.readline().split(':')[1].strip())
-        records = get_data(my_input.readline().split(':')[1].strip())
-
-    pairs = list(zip(times, records))
-    values = []
-    for pair in pairs:
-        values.append(calc_race(pair))
-
-    print(f'Result of day {DAY} part {SOLVE_PART} is {math.prod(values)}.')
+        for line in my_input:
+            hand, bid = line.rstrip().split(' ')
+            new_hand = Hand(hand, bid)
+    Hand.rank_of_hand()
+    print(f'Result of day {DAY} part {SOLVE_PART} is {Hand.count_bid()}.')
 
 
 def solve_02():
-    with open(FILE, 'r', encoding='utf-8') as my_input:
-        time = get_data(my_input.readline().split(':')[1].strip())
-        record = get_data(my_input.readline().split(':')[1].strip())
-    pair = (time, record)
-    result = calc_race(pair)
-    print(f'Result of day {DAY} part {SOLVE_PART} is {result}.')
+
+    Hand.type_of_cards = [
+        'J',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'T',
+        'Q',
+        'K',
+        'A',
+    ]
+    solve_01()
 
 
 def main():
+
     if SOLVE_PART == 1:
         solve_01()
     if SOLVE_PART == 2:
